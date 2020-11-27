6c1c5e8c33dfe1c16c0caa3e86090980a8180c8a
samir dangol <samirdangol@outlook.com>
2020-11-27 13:26:33 -0800

    additional attributes on weather

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
+import { Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap'
+
 const { REACT_APP_API_BASE_URL } = process.env;
 
 class App extends Component {
@@ -9,13 +12,10 @@ class App extends Component {
       weather: [],
       errorMessage: '',
       value: '',
-      responseMessage: ''
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);;
-
-
   }
 
   handleChange(event) {
@@ -38,10 +38,8 @@ class App extends Component {
           return Promise.reject(error);
         }
 
-        this.setState({ responseMessage: 'inserted' })
-        
         const newList = this.state.weather.filter(w => w.zipCode !== data.zipCode);
-        newList.push(data);
+        newList.unshift(data);
         this.setState({ weather: newList });
       })
       .catch(error => {
@@ -49,7 +47,7 @@ class App extends Component {
         console.error('There was an error!', error);
       });
 
-    
+
     event.preventDefault();
   }
 
@@ -74,33 +72,39 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
-    <h1>Hello, React! {REACT_APP_API_BASE_URL}</h1>
-
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Enter Zip:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-            </label>
-            <input type="submit" value="Add" />
-          </form>
-          <div>{this.state.value} {this.state.responseMessage}</div>
+        <h1>Hello Weather</h1>
+        <div className="row d-flex">
+          <div className="col-md-3 center">
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="formZip">
+                <Form.Control type="text" placeholder="Enter Zip" size="lg"
+                  value={this.state.value} onChange={this.handleChange} />
+              </Form.Group>
+              <Button type="submit">Search</Button>
+            </Form>
+          </div>
         </div>
 
+        <h3>Weather Results</h3>
         <div>
           {this.state.weather.map((data) => {
             return (
               <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">{data.zipCode}</h5>
-                  <p class="card-text">{data.temparature} F</p>
+                  <h5 class="card-title">{data.city}, {data.country} ({data.zipCode})</h5>
+                  <h6>{data.weatherDesc}</h6>
+                  <p class="card-text"><strong>{data.temp}°F</strong> temperature from <strong>{data.tempMin}°F</strong> to <strong>{data.tempMax}°F</strong>, wind <strong>{data.wind}</strong>. clouds <strong>{data.cloud}</strong>, <strong>{data.pressure}</strong></p>
+                  <h7>Geo coords [{data.latitude}, {data.longitude}], Last updated: {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', dayPeriod: 'short'}).format(new Date(data.asOfDate))}</h7>
                 </div>
               </div>
             );
           })}
         </div>
-      </div>
+
+        <div>
+          Azure Function URL:  {REACT_APP_API_BASE_URL}
+        </div>
+      </div >
     );
   }
 }
